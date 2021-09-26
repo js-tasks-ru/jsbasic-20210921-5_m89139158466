6609621e7e1d3ef5bcf164136e9e8d53129278ab
Mikhail Kolesnikov <m89139158466@gmail.com>
2021-09-26 22:35:23 +0700

    решил

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -8,13 +8,19 @@ function print(text) {
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
+ * имя не пустое, без пробелов, минимум 4 символа
  */
 function isValid(name) {
-  // ваш код...
-}
+  if (name == null) return false;
+  else{
+    return name.length > 3 && !name.includes(" ");
+  }
+ }
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
+
+
 
   if (isValid(userName)) {
     print(`Welcome back, ${userName}!`);
